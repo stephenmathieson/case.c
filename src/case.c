b9a1a00344c8c4de6640ae1c4e8521a72805b9b4
Mattias Andrée <maandree@kth.se>
2021-02-27 19:48:09 +0100

    Make functions inlineable
    Signed-off-by: Mattias Andrée <maandree@kth.se>

--- src/case.c
+++ src/case.c
@@ -6,49 +6,10 @@
 // MIT licensed
 //
 
-#include <ctype.h>
 #include "case.h"
 
-#define CASE_IS_SEP(c)    ((c) == '-' || (c) == '_' || (c) == ' ')
+extern inline char *case_upper(char *);
 
-char *
-case_upper(char *str) {
-  for (char *s = str; *s; s++) {
-    *s = toupper(*s);
-  }
-  return str;
-}
+extern inline char *case_lower(char *);
 
-char *
-case_lower(char *str) {
-  for (char *s = str; *s; s++) {
-    *s = tolower(*s);
-  }
-  return str;
-}
-
-char *
-case_camel(char *str) {
-  char *r = str, *w = str;
-  // never cap the first char
-  while (CASE_IS_SEP(*r)) {
-    r++;
-  }
-  while (*r && !CASE_IS_SEP(*r)) {
-    *w++ = *r++;
-  }
-  while (*r) {
-    do {
-      r++;
-    } while (CASE_IS_SEP(*r));
-    if (!*r)
-	    break;
-    *w++ = toupper(*r);
-    r++;
-    while (*r && !CASE_IS_SEP(*r)) {
-      *w++ = *r++;
-    }
-  }
-  *w = 0;
-  return str;
-}
+extern inline char *case_camel(char *);
--- src/case.h
+++ src/case.h
@@ -10,13 +10,51 @@
 #ifndef CASE_H
 #define CASE_H
 
-char *
-case_upper(char *);
+#include <ctype.h>
+#include "case.h"
 
-char *
-case_lower(char *);
+#define CASE_IS_SEP(c)    ((c) == '-' || (c) == '_' || (c) == ' ')
 
-char *
-case_camel(char *);
+inline char *
+case_upper(char *str) {
+  for (char *s = str; *s; s++) {
+    *s = toupper(*s);
+  }
+  return str;
+}
+
+inline char *
+case_lower(char *str) {
+  for (char *s = str; *s; s++) {
+    *s = tolower(*s);
+  }
+  return str;
+}
+
+inline char *
+case_camel(char *str) {
+  char *r = str, *w = str;
+  // never cap the first char
+  while (CASE_IS_SEP(*r)) {
+    r++;
+  }
+  while (*r && !CASE_IS_SEP(*r)) {
+    *w++ = *r++;
+  }
+  while (*r) {
+    do {
+      r++;
+    } while (CASE_IS_SEP(*r));
+    if (!*r)
+	    break;
+    *w++ = toupper(*r);
+    r++;
+    while (*r && !CASE_IS_SEP(*r)) {
+      *w++ = *r++;
+    }
+  }
+  *w = 0;
+  return str;
+}
 
 #endif
